fab0c5d110972ca718c28453fd7638688837a7f9
hevlyo <riquelme.pereira1@gmail.com>
2023-07-25 18:59:55 -0300

    Adding Try-Catch

--- Códigos/qntsAnosEm2022.js
+++ Códigos/qntsAnosEm2022.js
@@ -1,12 +1,20 @@
-const prompt = require('prompt-sync')();
+const prompt = require(`prompt-sync`)();
 
-const nome = prompt('Insira seu nome completo: ');
-let anoNascimento = parseInt(prompt('Insira seu ano de nascimento: '));
+const nome = prompt(`Insira seu nome completo: `);
+let anoNascimento = parseInt(prompt(`Insira seu ano de nascimento: `));
 
-while (anoNascimento < 1922 || anoNascimento > 2021) {
-    anoNascimento = parseInt(prompt('Insira seu ano de nascimento: '));
+while(true) {
+    try {
+        if(anoNascimento == ``) throw `Está vazio`;
+        if(isNaN(anoNascimento)) throw `Não é um número`;
+        if(anoNascimento < 1922 || anoNascimento > 2021) throw `Ano de nascimento inválido`;
+        break;
+      }
+      catch(err) {
+        console.log(err);
+      }
+    anoNascimento = parseInt(prompt(`Insira seu ano de nascimento: `));
 }
 
 const idadeProxAno = -(anoNascimento - 2022);
-
 console.log(`Seu nome é ${nome} e você completará ${idadeProxAno} anos em 2022.`);

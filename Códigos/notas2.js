b700c165d0f77a48094d7225d0fdb4cf34fe2aa7
hevlyo <riquelme.pereira1@gmail.com>
2023-08-21 12:23:28 -0300

    Update notas2.js

--- Códigos/notas2.js
+++ Códigos/notas2.js
@@ -3,6 +3,6 @@ const readline = require('readline-sync');
 const nota1 = readline.questionFloat('Digite a primeira nota: ');
 const nota2 = readline.questionFloat('Digite a segunda nota: ');
 
-const notaMinima = 14 - nota1 - nota2;
+const notaMinima = 21 - (nota1 + nota2);
 
 console.log(`Nota mínima necessária na próxima prova: ${notaMinima}`);

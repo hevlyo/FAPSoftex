50bcf1163ab2484633ae66967cc9a39c3db9468b
hevlyo <riquelme.pereira1@gmail.com>
2023-09-05 17:07:34 -0300

    Add ehParOuImpar.js

---
+++ Códigos/ehParOuImpar.js
@@ -0,0 +1,12 @@
+const readline = require('readline-sync');
+
+function ehParOuImpar(numero) {
+    if (numero % 2 == 0) {
+        return "par";
+    } else {
+        return "impar";
+    }
+}
+
+numero = readline.questionInt("Digite um número: ");
+console.log(`O número ${numero} é ${ehParOuImpar(numero)}`);

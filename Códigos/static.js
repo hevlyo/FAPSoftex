62e0182e2170bb4d285887a6a09a8f13bddc40f5
hevlyo <riquelme.pereira1@gmail.com>
2023-09-05 16:48:37 -0300

    Add static.js

---
+++ Códigos/static.js
@@ -0,0 +1,9 @@
+class Socorro {
+    static socorro = 'Ninguem nunca vai me ajudar?';
+    static help() {
+        return 'Alguém me ajuda por favor'
+    }
+}
+
+console.log(Socorro.socorro);
+console.log(Socorro.help());

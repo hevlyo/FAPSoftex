046d8dc8110a283101c7859c2c0ade34ad476c81
hevlyo <riquelme.pereira1@gmail.com>
2023-09-11 14:00:30 -0300

    Adding collections.js

---
+++ Códigos/collections.js
@@ -0,0 +1,9 @@
+const pontuacoes = new Map([
+    ["João", 50],
+    ["Pedro", 120],
+    ["Luiza", 70],
+    ["Marcela", 10],
+    ["Yuri", 34]
+]);
+
+console.log("Pontuação de João:", pontuacoes.get("João"));

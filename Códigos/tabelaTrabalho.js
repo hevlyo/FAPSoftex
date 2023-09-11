a6e39b6ae4448a422742a3554626883d9b5cb1e8
hevlyo <riquelme.pereira1@gmail.com>
2023-09-11 13:28:34 -0300

    Adding tabelaTrabalho.js

---
+++ Códigos/tabelaTrabalho.js
@@ -0,0 +1,26 @@
+const pessoas = [
+    {
+        Nome: 'José',
+        Idade: 19,
+        Salário: 1900,
+        'Tem Ensino Médio Completo?': true
+    },
+    {
+        Nome: 'David',
+        Idade: 22,
+        Salário: 2500,
+        'Tem Ensino Médio Completo?': false
+    },
+    {
+        Nome: 'Maria',
+        Idade: 25,
+        Salário: 3400,
+        'Tem Ensino Médio Completo?': true
+    },
+    {
+        Nome: 'Igor',
+        Idade: 18,
+        Salário: 2100,
+        'Tem Ensino Médio Completo?': false
+    }
+];

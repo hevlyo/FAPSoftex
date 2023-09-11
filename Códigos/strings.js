a614bc44be1e93205acd56cd785e242caae37552
hevlyo <riquelme.pereira1@gmail.com>
2023-09-11 13:56:26 -0300

    Update strings.js

--- Códigos/strings.js
+++ Códigos/strings.js
@@ -4,3 +4,12 @@ const words = frase.split(" ");
 
 console.log("Palavras separadas:");
 console.log(words);
+
+// String de exemplo com espaços em branco no início e no final
+const stringWithWhitespace = "   Olá, mundo!   ";
+
+// Usando o método trim para remover espaços em branco
+const trimmedString = stringWithWhitespace.trim();
+
+console.log("String original:", stringWithWhitespace);
+console.log("String após o trim:", trimmedString);
